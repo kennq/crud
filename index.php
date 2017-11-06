8b21c2049d62c82ea960d8d06a79d3715c32905a
kennq <kent_zxc04@yahoo.com>
2017-11-07 01:18:58 +0800

    comit

--- index.php
+++ index.php
@@ -2,11 +2,12 @@
 <html>
 <head>
 <title>Crud </title>
-<form action ="" method="">
+<form action ="" method="POST">
+	<label for="firstname">Firstname</label>
+	<input type="text" name="firstname">
+	
 </form>
 </head>
 <body>
-
-
 </body>
 </html>
